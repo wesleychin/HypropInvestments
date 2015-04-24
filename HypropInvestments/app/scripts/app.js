ac76fb99c51c01e74d07619a79fdf79ab3ea09d4
Wesley Chin <wesley@databuild.co.za>
2015-04-24 11:31:53 +0200

    Added basic nav

--- HypropInvestments/app/scripts/app.js
+++ HypropInvestments/app/scripts/app.js
@@ -10,10 +10,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'highcharts-ng'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
+  if (window.cordova && window.cordova.plugins.Keyboard) {
+    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  }
+  if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
@@ -47,15 +47,34 @@ angular.module('starter', ['ionic', 'starter.controllers', 'highcharts-ng'])
       }
     }
   })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
+
+  .state('app.home', {
+    url: "/home",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/home.html"
       }
-    })
+    }
+  })
+
+  .state('app.shareinformation', {
+    url: "/shareinformation",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/shareinformation.html"
+      }
+    }
+  })
+
+  .state('app.playlists', {
+    url: "/playlists",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlists.html",
+        controller: 'PlaylistsCtrl'
+      }
+    }
+  })
 
   .state('app.single', {
     url: "/playlists/:playlistId",
@@ -67,5 +86,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'highcharts-ng'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
